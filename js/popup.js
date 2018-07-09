d4f1be4f0e577c84cabb2696ccf18ee08d27b147
naganao <32755305+naganao@users.noreply.github.com>
2018-07-09 19:51:23 +0900

    一通り実装
    一通り実装
    もっとこまめにコミットすべきだと思う。

--- js/popup.js
+++ js/popup.js
@@ -2,8 +2,81 @@ $(function () {
   chrome.tabs.getSelected(null, function(tab) {
     $('#title').text(tab.title);
     $('#url').text(tab.url);
-    $("body").html(
-      $("body").html().replace( /ー/g, "━━━(ﾟ∀ﾟ)━━━" )
-    );
+
+    // chrome.tabs.sendMessage(tab.url, function (response){
+    // 	console.log("受け取ったデータ：", response)
+    // })
+
+    if (localStorage.getItem('Amazon') === null) {
+      localStorage.setItem('Amazon', 'unchecked');
+    }
+
+    if (localStorage.getItem('YouTube') === null) {
+      localStorage.setItem('YouTube', 'unchecked');
+    }
+
+    var Amazon = localStorage.getItem('Amazon');
+    var YouTube = localStorage.getItem('YouTube');
+    console.log(Amazon);
+    console.log(YouTube);
+
+    if (Amazon.indexOf('checked') == 0) {
+      $('#Amazon').prop('checked',true);
+
+      if (tab.url.indexOf('http://www.amazon.co.jp') == 0 ||
+      tab.url.indexOf('https://www.amazon.co.jp') == 0) {
+        // Amazon のページのときの処理
+        $('#caution').text("遊ぶな！");
+
+      }
+
+    } else {
+      $('#Amazon').prop('checked',false);
+    }
+
+    if (YouTube.indexOf('checked') == 0) {
+      $('#YouTube').prop('checked',true);
+
+      if (tab.url.indexOf('http://www.youtube.com') == 0 ||
+      tab.url.indexOf('https://www.youtube.com') == 0) {
+        // YouTube のページのときの処理
+        $('#caution').text("遊ぶな！");
+      }
+
+    } else {
+      $('#YouTube').prop('checked',false);
+    }
+
   });
 });
+
+$(function() {
+  // チェックボックスをチェックしたら発動
+  $('input[name="site"]').change(function() {
+
+    var Amazon = $('#Amazon').prop('checked');
+    var YouTube = $('#YouTube').prop('checked');
+
+    if (Amazon) {
+      localStorage.setItem('Amazon', 'checked');
+    } else {
+      localStorage.setItem("Amazon", "unchecked");
+    }
+
+    if (YouTube) {
+      localStorage.setItem('YouTube', 'checked');
+    } else {
+      localStorage.setItem("YouTube", "unchecked");
+    }
+
+  });
+});
+
+function checkSite(site_name) {
+
+}
+
+// function () {
+//   var site_list = [];
+//
+// }
---
+++ script.js
@@ -0,0 +1,45 @@
+$(function(){
+  chrome.runtime.sendMessage({method: "getLocalStorage", key: "Amazon"}, function(response) {
+    console.log(response.data);
+    if (response.data.indexOf('checked') == 0) {
+      var tabURL = window.location.href;
+      //console.log(tabURL);
+      if (tabURL.indexOf('http://www.amazon.co.jp') == 0 ||
+      tabURL.indexOf('https://www.amazon.co.jp') == 0) {
+        // Amazon のページのときの処理
+        disp();
+      } else {
+        //$('#Amazon').prop('checked',false);
+      }
+    }
+  });
+
+  chrome.runtime.sendMessage({method: "getLocalStorage", key: "YouTube"}, function(response) {
+    if (response.data.indexOf('checked') == 0) {
+      var tabURL = window.location.href;
+
+      if (tabURL.indexOf('http://www.youtube.com') == 0 ||
+      tabURL.indexOf('https://www.youtube.com') == 0) {
+        // YouTube のページのときの処理
+        disp();
+      }
+
+    } else {
+      //$('#YouTube').prop('checked',false);
+    }
+
+  });
+});
+
+function disp(){
+  // 「OK」時の処理開始 ＋ 確認ダイアログの表示
+  if(window.confirm('遊ぶんじゃない！')){
+    history.back();
+  }
+  // 「OK」時の処理終了
+  // 「キャンセル」時の処理開始
+  else{
+    window.alert('キャンセルされました'); // 警告ダイアログを表示
+  }
+  // 「キャンセル」時の処理終了
+}
